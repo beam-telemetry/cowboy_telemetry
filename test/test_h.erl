b6da97606524b30797b6d3acdebc8a495186719d
Vince Foley <vincefoley@gmail.com>
2020-06-22 15:39:33 -0700

    correct indentation

--- test/test_h.erl
+++ test/test_h.erl
@@ -4,6 +4,6 @@
 -export([init/2]).
 
 init(_, failure) ->
-  error(failure);
+    error(failure);
 init(Req, success = Opts) ->
-  {ok, cowboy_req:reply(200, #{}, <<"Hello world!">>, Req), Opts}.
+    {ok, cowboy_req:reply(200, #{}, <<"Hello world!">>, Req), Opts}.
