9cbf3ff285faff7e937cd468ca70396d649fd03a
Vince Foley <vincefoley@gmail.com>
2020-06-24 23:41:46 -0700

    pull telemetry calls into functions

--- src/cowboy_telemetry_h.erl
+++ src/cowboy_telemetry_h.erl
@@ -8,13 +8,7 @@
 -export([early_error/5]).
 
 init(StreamID, Req, Opts) ->
-    SystemTime = erlang:system_time(),
-    StartTime = erlang:monotonic_time(),
-    telemetry:execute(
-        [cowboy, request, start],
-        #{system_time => SystemTime},
-        #{stream_id => StreamID, req => Req}
-    ),
+    StartTime = emit_start_event(StreamID, Req),
     {Commands, Next} = cowboy_stream:init(StreamID, Req, Opts),
     {Commands, [Next | StartTime]}.
 
@@ -27,22 +21,12 @@ info(StreamID, Info, [Next0 | StartTime]) ->
     State =
         case Commands of
             [{response, _, _, _} = Response] ->
-                EndTime = erlang:monotonic_time(),
-                telemetry:execute(
-                    [cowboy, request, stop],
-                    #{duration => EndTime - StartTime},
-                    #{stream_id => StreamID, response => Response}
-                ),
+                emit_stop_event(StreamID, StartTime, Response),
                 done;
             [{error_response, _, _, _} = ErrorResponse | Commands1] ->
-                EndTime = erlang:monotonic_time(),
                 case lists:keyfind(internal_error, 1, Commands1) of
                     {internal_error, {'EXIT', _, Reason}, _} ->
-                        telemetry:execute(
-                            [cowboy, request, exception],
-                            #{duration => EndTime - StartTime},
-                            #{stream_id => StreamID, kind => exit, reason => Reason, error_response => ErrorResponse}
-                        ),
+                        emit_exception_event(StreamID, StartTime, Reason, ErrorResponse),
                         done;
                     _ ->
                         StartTime
@@ -57,27 +41,60 @@ terminate(StreamID, Reason, [Next | done]) ->
 terminate(StreamID, Reason, [Next | StartTime]) ->
     case Reason of
         {socket_error, _, _} = Reason ->
-            emit_early_terminate_error_event(StreamID, StartTime, Reason);
+            emit_stop_error_event(StreamID, StartTime, Reason);
         {connection_error, _, _} = Reason ->
-            emit_early_terminate_error_event(StreamID, StartTime, Reason);
+            emit_stop_error_event(StreamID, StartTime, Reason);
         _ -> ignore
     end,
     cowboy_stream:terminate(StreamID, Reason, Next).
 
 early_error(StreamID, Reason, PartialReq, Resp0, Opts) ->
-    SystemTime = erlang:system_time(),
     Resp = cowboy_stream:early_error(StreamID, Reason, PartialReq, Resp0, Opts),
-    telemetry:execute(
-        [cowboy, request, early_error],
-        #{system_time => SystemTime},
-        #{stream_id => StreamID, reason => Reason, partial_req => PartialReq, response => Resp}
-    ),
+    emit_early_error_event(StreamID, Reason, PartialReq, Resp),
     Resp.
 
-emit_early_terminate_error_event(StreamID, StartTime, Reason) ->
+% Telemetry events
+
+emit_start_event(StreamID, Req) ->
+    SystemTime = erlang:system_time(),
+    StartTime = erlang:monotonic_time(),
+    telemetry:execute(
+        [cowboy, request, start],
+        #{system_time => SystemTime},
+        #{stream_id => StreamID, req => Req}
+    ),
+    StartTime.
+
+emit_stop_event(StreamID, StartTime, Response) ->
     EndTime = erlang:monotonic_time(),
     telemetry:execute(
         [cowboy, request, stop],
         #{duration => EndTime - StartTime},
+        #{stream_id => StreamID, response => Response}
+    ).
+
+emit_stop_error_event(StreamID, StartTime, Reason) ->
+    EndTime = erlang:monotonic_time(),
+    io:format("~p~n", [Reason]),
+    telemetry:execute(
+        [cowboy, request, stop],
+        #{duration => EndTime - StartTime},
         #{stream_id => StreamID, error => Reason}
+    ).
+
+emit_exception_event(StreamID, StartTime, Reason, ErrorResponse) ->
+    EndTime = erlang:monotonic_time(),
+    telemetry:execute(
+        [cowboy, request, exception],
+        #{duration => EndTime - StartTime},
+        #{stream_id => StreamID, kind => exit, reason => Reason, error_response => ErrorResponse}
+    ).
+
+
+emit_early_error_event(StreamID, Reason, PartialReq, Resp) ->
+    SystemTime = erlang:system_time(),
+    telemetry:execute(
+        [cowboy, request, early_error],
+        #{system_time => SystemTime},
+        #{stream_id => StreamID, reason => Reason, partial_req => PartialReq, response => Resp}
     ).
