e68c1258c047e05d0378049170cda42792366905
Vince Foley <vincefoley@gmail.com>
2020-06-24 21:16:35 -0700

    Mark state when ending event is already sent

--- src/cowboy_telemetry_h.erl
+++ src/cowboy_telemetry_h.erl
@@ -24,31 +24,36 @@ data(StreamID, IsFin, Data, [Next0 | StartTime]) ->
 
 info(StreamID, Info, [Next0 | StartTime]) ->
     {Commands, Next} = cowboy_stream:info(StreamID, Info, Next0),
-    case Commands of
-        [{response, _, _, _} = Response] ->
-            EndTime = erlang:monotonic_time(),
-            telemetry:execute(
-                [cowboy, request, stop],
-                #{duration => EndTime - StartTime},
-                #{stream_id => StreamID, response => Response}
-            );
-        [{error_response, _, _, _} = ErrorResponse | Commands1] ->
-            EndTime = erlang:monotonic_time(),
-            case lists:keyfind(internal_error, 1, Commands1) of
-                {internal_error, {'EXIT', _, Reason}, _} ->
-                    telemetry:execute(
-                        [cowboy, request, exception],
-                        #{duration => EndTime - StartTime},
-                        #{stream_id => StreamID, kind => exit, reason => Reason, error_response => ErrorResponse}
-                    );
-                _ ->
-                    ignore
-            end;
-        _ ->
-            ignore
-    end,
-    {Commands, [Next | StartTime]}.
+    State =
+        case Commands of
+            [{response, _, _, _} = Response] ->
+                EndTime = erlang:monotonic_time(),
+                telemetry:execute(
+                    [cowboy, request, stop],
+                    #{duration => EndTime - StartTime},
+                    #{stream_id => StreamID, response => Response}
+                ),
+                done;
+            [{error_response, _, _, _} = ErrorResponse | Commands1] ->
+                EndTime = erlang:monotonic_time(),
+                case lists:keyfind(internal_error, 1, Commands1) of
+                    {internal_error, {'EXIT', _, Reason}, _} ->
+                        telemetry:execute(
+                            [cowboy, request, exception],
+                            #{duration => EndTime - StartTime},
+                            #{stream_id => StreamID, kind => exit, reason => Reason, error_response => ErrorResponse}
+                        ),
+                        done;
+                    _ ->
+                        StartTime
+                end;
+            _ ->
+                StartTime
+        end,
+    {Commands, [Next | State]}.
 
+terminate(StreamID, Reason, [Next | done]) ->
+    cowboy_stream:terminate(StreamID, Reason, Next);
 terminate(StreamID, Reason, [Next | StartTime]) ->
     case Reason of
         {socket_error, _, _} = Reason ->
